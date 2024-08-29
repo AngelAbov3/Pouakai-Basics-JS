682d736f9c11b3caf5846d9951fbfb42cc67f8d3
Josh <JoshMcGeeNZ@outlook.com>
2024-08-29 13:00:16 +1200

    Js Basics

--- JS/basics.js
+++ JS/basics.js
@@ -2,24 +2,38 @@
 
 // 1. Write a variable named myName and make it a string of your name
 
+var myName = 'Josh'
+console.log(myName)
 
 // 2. Write a variable named age and make it a number of your age
 
+var age = '19'
+console.log(age)
 
 // 3. Write a variable named isStudent and make it a boolean that represents your status as a student
 
+var isStudent = 'false'
+console.log(isStudent)
 
 // 4. Write a variable named favoriteColor and leave it undefined
 
+var favoriteColor = ''
+console.log(favoriteColor)
 
 // 5. Write a variable named greeting and set it to "Hello, world!"
 
+var greeting = 'Hello World'
+console.log(greeting)
 
 // 6. Write a variable named myUndefined and leave it undefined
 
+var myUndefined = ''
+console.log(myUndefined)
 
 // 7. Write a variable named myFavColor and make it a string of your favorite color
 
+var myFavColor = 'Purple'
+console.log(myFavColor)
 
 // 8. Define your favoriteColor variable as myFavColor
 
